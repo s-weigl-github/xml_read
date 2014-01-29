4ae2c22fafedc6f07b469dfaa47a8fd262bd468e
s.weigl.git <s.weigl.web@googlemail.com>
2014-01-30 00:25:17 +0100

    changed somethings, added bday calculation
    this work's for now

--- poc02.py
+++ poc02.py
@@ -1,11 +1,20 @@
 import datetime, time
+from xml.etree.ElementTree import parse
 
 utc_dt = datetime.datetime.now()
 s = utc_dt.strftime('%d.%m.')
 atc_dt = datetime.datetime(2014, 1, 26)
 e = atc_dt.strftime('%d.%m.')
 
-if s == e:
-  print("both data sets are equal")
-else:
-  print("data set is not equal")
+doc = parse('daten.swxml')
+for person in doc.findall('person'):
+  bday = person.findtext('bday')
+  bday = int(bday)
+  bday_dt = datetime.datetime(bday)
+  bday = bday_dt.strftime('%d.%m.')
+  print(bday)
+
+##if s == e:
+##  print("both data sets are equal")
+##else:
+##  print("data set is not equal")
--- xml_read.py
+++ xml_read.py
@@ -1,22 +1,40 @@
 #########################
 ###  read and compute datasets from a xml file
 ###  Sebastian Weigl
-###  ver.02 - 27.01.2014
+###  ver.05 - 30.01.2014
 #########################
 import urllib.request
 from xml.etree.ElementTree import parse
 import time, datetime
 import webbrowser
 
-dt = datetime.datetime.now()
-s = dt.strftime("%d.%m.")
+dt = datetime.datetime(2014, 10, 6)
+s = dt.strftime("%d")
+t = dt.strftime("%m")
+u = dt.strftime("%Y")
+u = int(u)
+
+wert = 0
+age = None
+ye_ar = None
 
 doc = parse('daten.swxml')
 for person in doc.findall('person'):
-  bday = person.findtext('bday')
+  da_y = person.findtext('da_y')
+  mon_th = person.findtext('mon_th')
+  ye_ar = person.findtext('ye_ar')
+  ye_ar = int(ye_ar)
   pid = person.findtext('id')
   name = person.findtext('name')
   surname = person.findtext('surname')
   gender = person.findtext('gender')
-  if gender.startswith('f') or gender.startswith('m'):
-    print(surname+', '+name,'| ID', pid,'\n', bday+' - '+gender)
+##  if gender.startswith('f') or gender.startswith('m'):
+##    print(surname+', '+name,'| ID', pid,'\n', da_y+'.'+mon_th+'.'+ye_ar+' - '+gender)
+  if da_y == s and mon_th == t:
+    age = u - ye_ar
+    if u - ye_ar == age:
+      age = str(age)
+      print("Geburtstag - ist Heute "+age+" geworden ", da_y+'.'+mon_th+'. -', name)
+  else:
+    wert = wert + 1
+    print(wert, "dauert noch")
