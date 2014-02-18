0ccca3ff9b1db5aa20e993e15b4f0f875a0f0f88
s.weigl.git <s.weigl.web@googlemail.com>
2014-02-18 17:50:38 +0100

    ground layout
    python script to write datasets into a xml file

---
+++ xml_write.py
@@ -0,0 +1,10 @@
+#########################
+###  write datasets to a xml file
+###  Sebastian Weigl
+###  ver.01 alpha - 16.02.2014
+#########################
+import urllib.request
+from xml.etree.ElementTree import parse
+
+
+## E-o-F
