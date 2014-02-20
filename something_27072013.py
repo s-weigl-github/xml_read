d0ba94ba688dac519c8c5ec16ca29c21211822a3
s.weigl.git <s.weigl.web@googlemail.com>
2014-02-20 16:55:36 +0100

    just test output

--- something_27072013.py
+++ something_27072013.py
@@ -1,2 +1,21 @@
+Device = 20
+Total = 30
+Used = 40
+Free = 50
+Use = 60
+Type = 70
+Mount = 80
+t = 0
+
 templ = "%-10s %8s %8s %8s %5s%% %9s  %s"
 print(templ % ("Device", "Total", "Used", "Free", "Use ", "Type", "Mount"))
+while t < 30:
+  print(templ % (Device, Total, Used, Free, Use, Type, Mount))
+  t = t + 1
+  Device = Device+10
+  Total = Total+10
+  Used = Used+10
+  Free = Free+10
+  Use = Use+10
+  Type = Type+10
+  Mount = Mount+10
