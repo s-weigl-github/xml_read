9a4a78f2ab0468c5037b44a9f0357c911c9a8536
s.weigl.git <s.weigl.web@googlemail.com>
2014-02-22 15:05:35 +0100

    added info,, cleaned code

--- something_20022014.py
+++ something_20022014.py
@@ -1,3 +1,8 @@
+#####################################
+###  compute and write to file
+###  Sebastian Weigl
+###  ver.1 - 22.02.2014
+#####################################
 import os
 
 Anzahl = 0
@@ -5,13 +10,16 @@ Einzel = 30
 MWST = 0.19
 Gesamt = 0
 t = 0
+numb = 500 #numb def how often the loop will be execute
+
+open('berechnung.txt','w').close() #open a file and close it right a way and leave it empty
 
 def ptf():
-  text = open('berechnung.txt', 'a')
+  text = open('berechnung.txt','a')
   text.write(ausgabe)
   text.close()
 def pfl():
-  text = open('berechnung.txt', 'a')
+  text = open('berechnung.txt','a')
   text.write(firstline)
   text.close()
 
@@ -22,7 +30,7 @@ print(templ % ("Anzahl", "Einzel Preis", "MWST", "Gesamt Preis"))
 firstline = str(templ % ("Anzahl", "Einzel Preis", "MWST", "Gesamt Preis")+'\n'+'\n')
 pfl()
 
-while t < 56:
+while t < numb:
   t = t + 1
   Anzahl = Anzahl+10
   Aufs = Anzahl*Einzel
