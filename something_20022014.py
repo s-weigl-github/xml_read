36768d3075518417960318e327ce31701d9a1eec
s.weigl.git <s.weigl.web@googlemail.com>
2014-02-22 14:05:52 +0100

    strugling with the right output (not working)

--- something_20022014.py
+++ something_20022014.py
@@ -6,21 +6,25 @@ MWST = 0.19
 Gesamt = 0
 t = 0
 
+def ptf():
+  text = open('berechnung.txt', 'w')
+  text.write(ausgabe)
+  text.close()
+
 temp2 = "%5s"
 templ = "%10s %15s %8s %% %15s €"
+
 print(templ % ("Anzahl", "Einzel Preis", "MWST", "Gesamt Preis"))
+
 while t < 56:
   t = t + 1
+  ausgabe = str(''+templ % (Anzahl, Einzel, MWST, Gesamt)+'\n')
+  ptf()
   Anzahl = Anzahl+10
   Aufs = Anzahl*Einzel
   Gesamt = Aufs+Aufs*MWST
   print(templ % (Anzahl, Einzel, MWST, Gesamt))
-  ausgabe = (templ % (Anzahl, Einzel, MWST, Gesamt))
 
-  text = open('berechnung.txt', 'w')
-  text.write(ausgabe)
-  text.close()
-  
 print("-------------------", temp2 % ("Done!"), "-------------------")
 
 ## E-o-F
