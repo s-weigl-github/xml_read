00fcbc3c3ca494d8befe775e0fa5bf6ad2a7d261
s.weigl.git <s.weigl.web@googlemail.com>
2014-02-22 14:22:24 +0100

    output to file with text struct (now works)

--- something_20022014.py
+++ something_20022014.py
@@ -7,23 +7,29 @@ Gesamt = 0
 t = 0
 
 def ptf():
-  text = open('berechnung.txt', 'w')
+  text = open('berechnung.txt', 'a')
   text.write(ausgabe)
+  text.close()
+def pfl():
+  text = open('berechnung.txt', 'a')
+  text.write(firstline)
   text.close()
 
 temp2 = "%5s"
 templ = "%10s %15s %8s %% %15s €"
 
 print(templ % ("Anzahl", "Einzel Preis", "MWST", "Gesamt Preis"))
+firstline = str(templ % ("Anzahl", "Einzel Preis", "MWST", "Gesamt Preis")+'\n'+'\n')
+pfl()
 
 while t < 56:
   t = t + 1
-  ausgabe = str(''+templ % (Anzahl, Einzel, MWST, Gesamt)+'\n')
-  ptf()
   Anzahl = Anzahl+10
   Aufs = Anzahl*Einzel
   Gesamt = Aufs+Aufs*MWST
   print(templ % (Anzahl, Einzel, MWST, Gesamt))
+  ausgabe = str(templ % (Anzahl, Einzel, MWST, Gesamt)+'\n')
+  ptf()
 
 print("-------------------", temp2 % ("Done!"), "-------------------")
 
