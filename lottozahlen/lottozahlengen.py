5c731f739d6410c8eba0e291ccb05c0ac4cb40ef
s.weigl.git <s.weigl.web@googlemail.com>
2014-06-08 03:37:49 +0200

    changed the code

--- lottozahlen/lottozahlengen.py
+++ lottozahlen/lottozahlengen.py
@@ -1,9 +1,10 @@
 ####################
 ###  lottozahlen generator
-###  S.Weigl - 10.05.2014
-###  ver. 02 - beta
+###  S.Weigl - 08.06.2014
+###  ver. 01 - beta
 ####################
 
+from xml.etree.ElementTree import parse
 import random
 
 rerun = 0
@@ -13,12 +14,25 @@ def lotto():
   count = 6
   while wert < count:
     wert = wert + 1
-    print("  ",random.choice([3,6,9,11,17,22,25,26,31,32,33,36,38,43,49]))
-  print("-------------------")
+    print(" ", random.choice([6,11,22,26,31,32,33,38,43,49]))    
+  print("\n-------------------\n")
   print("Done!")
 
-while rerun != 5:
-  rerun = rerun + 1
-  lotto()
+doc = parse('lottozahlen.xml')
+for zahlen in doc.findall('zahlen'):
+  kugelwert = zahlen.findtext('numb')
+  kugelwert = int(kugelwert)
+  gezogen = zahlen.findtext('picked')
+  gezogen = int(gezogen)
 
-## E-o-F
+  while rerun != 1:
+    rerun = rerun + 1
+    print("-------------------\n")
+    lotto()
+
+## E-o-F    
+
+  #if gezogen > 475:
+    #print(gezogen, kugelwert)
+    #print("die am meist gezogenen lottozahlen")
+    
