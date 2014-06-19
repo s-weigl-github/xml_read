889bdb4b9178b88cdb4035042a64a5c5cbe2b247
s.weigl.git <s.weigl.web@googlemail.com>
2014-06-19 19:35:58 +0200

    updated numbers and changed some little things

--- lottozahlen/lottozahlengen.py
+++ lottozahlen/lottozahlengen.py
@@ -1,11 +1,12 @@
 ####################
 ###  lottozahlen generator
-###  S.Weigl - 08.06.2014
-###  ver. 01 - beta
+###  S.Weigl - 19.06.2014
+###  ver. 01
 ####################
 
 from xml.etree.ElementTree import parse
 import random
+import sys
 
 rerun = 0
 
@@ -14,8 +15,8 @@ def lotto():
   count = 6
   while wert < count:
     wert = wert + 1
-##    print(" ", random.choice([6,11,22,26,31,32,33,38,43,49]))
-    print(" ", random.choice([3,6,9,11,17,22,25,26,27,31,32,33,36,38,42,43,49]))
+    wahl = random.choice([3,6,9,11,17,22,25,26,31,32,33,36,38,42,43,49])
+    print(" ", wahl)
   print("\n-------------------\n")
   print("Done!")
 
