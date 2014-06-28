fa011f126973b6b4681b2b449dae14a3682197d2
s.weigl.git <s.weigl.web@googlemail.com>
2014-06-28 22:36:41 +0200

    updated numbers

--- lottozahlen/lottozahlengen.py
+++ lottozahlen/lottozahlengen.py
@@ -15,7 +15,8 @@ def lotto():
   count = 6
   while wert < count:
     wert = wert + 1
-    wahl = random.choice([3,6,9,11,17,22,25,26,31,32,33,36,38,42,43,49])
+    wahl = random.choice([6,11,22,26,31,32,33,38,43,49])
+#    wahl = random.choice([3,6,9,11,17,22,25,26,31,32,33,36,38,42,43,49])
     print(" ", wahl)
   print("\n-------------------\n")
   print("Done!")
@@ -34,6 +35,6 @@ for zahlen in doc.findall('zahlen'):
 
 ## E-o-F    
 
-##  if gezogen > 471:
+##  if gezogen > 476:
 ##    print(gezogen, kugelwert)
 ##    print("die am meist gezogenen lottozahlen")
